85935aba7118827a469879f0821df815e42746db
Eric Wee <ewee@au.westfield.com>
2014-03-25 22:52:28 +1100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TenPinBowling::Application.config.secret_key_base = '5e7879138f47d7e1c940d681fff971a3f15718ebbe301f21b032d097e84bf6b1c702f5252d1c5ddddd39cf0215e653b53f4feb90a3e793e365a93c38c6aae24e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TenPinBowling::Application.config.session_store :cookie_store, key: '_ten-pin-bowling_session'
